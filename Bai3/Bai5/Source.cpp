c663bb940b515bb89d58883d92997c6a5eb2b0fe
Le Hoang Nam <lehoangnamm1997@gmail.com>
2019-07-17 16:49:11 +0700

    bai3

--- Bai3/Bai5/Source.cpp
+++ Bai3/Bai5/Source.cpp
@@ -31,3 +31,42 @@ int main() {
 
 	system("pause");
 }
+
+
+#include <iostream>;
+#include <math.h>;
+
+//using namespace std;
+//int main() {
+//	int n;
+//	do {
+//		cout << "nhap so duong n";
+//		cin >> n;
+//	} while (n < 0);
+//	if (n == 1)
+//	{
+//		cout << "n k phai la nguyen to";
+//	}
+//	else {
+//		int dem = 0;
+//		int i = 2;
+//		while (i < n) {
+//			if (n%i == 0) {
+//				dem++;
+//			}
+//			i = i + 1;
+//		}
+//		if (dem == 0) {
+//			cout << "\n day la so ngyen to";
+//		}
+//		else {
+//			cout << "\n k phai so ng to";
+//
+//		}
+//
+//
+//	}
+//
+//
+//	system("pause");
+//}
